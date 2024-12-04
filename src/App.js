42250d3231688873ff52394c989057a4378497c9
Mert-Ozbolat <mertozbolat008@gmail.com>
2024-12-04 18:00:05 +0300

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Main from './Components/Main/Main';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import FilteredProducts from './Components/FiltredProducts/FilteredProducts';
+import SingleProduct from './Components/FiltredProducts/SingleProduct';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Main></Main>} />
           <Route path='/filteredProducts/:type' element={<FilteredProducts />} />
+          <Route path='/filteredProduct/:type/:id' element={<SingleProduct />} />
         </Routes>
       </BrowserRouter>
     </div>
